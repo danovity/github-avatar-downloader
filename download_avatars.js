7132d7254e8d2472b3a7b94cffd51f702768adec
danovity <sean.xiaoxu.ye@gmail.com>
2018-08-06 14:18:37 -0400

    added all images

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require("request");
 var token = require("./secrets");
+var fs = require("fs");
 
 function getRepoContributors(repoOwner, repoName, cb) {
   console.log("triggered.");
@@ -11,17 +12,44 @@ function getRepoContributors(repoOwner, repoName, cb) {
       repoName +
       "/contributors",
     headers: {
-      "User-Agent": "request"
+      "User-Agent": "request",
+      Authorization: "token" + token.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  //console.log("Result:", result);
+  result.forEach((cur, i) => {
+    console.log(cur.avatar_url);
+    //downloadImageByURL(cur.avatar_url, `./avatars/img[${i}].jpg`);
+    downloadImageByURL(cur.avatar_url, `./avatars/img${i}.jpg`);
+  });
 });
 //console.log("Welcome to the GitHub Avatar Downloader!");
+
+function downloadImageByURL(url, filePath) {
+  // ...
+  request
+    .get(url) // Note 1
+    .on("error", function(err) {
+      // Note 2
+      throw err;
+    })
+    .on("response", function(response) {
+      // Note 3
+      console.log("Response Status Code: ", response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath)); // Note 4
+
+  // Notes:
+  // 1. `request.get` is equivalent to `request()`
+  // 2. `request.on('error', callback)` handles any error
+  // 3. `request.on('response, callback)` handles the response
+  // 4. What is happening here?
+}
